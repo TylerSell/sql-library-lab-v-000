6ad30b6eeab40d5a57476862920ed5d91ca8d0ca
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 06:26:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
   FROM authors 
   JOIN series 
   ON authors.id = series.author_id 
-  JOIN subgenress
+  JOIN subgenres
   ON subgenres.id = series.subgenres_id;"
 end
 
