9f5d47b52f37760762fae88cb2ff9a188a31d5cc
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 05:57:15 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,9 +1,13 @@
 CREATE TABLE series (
 id INTEGER PRIMARY KEY,
-
+title TEXT,
+author_id INTEGER,
+sub_genre_id INTEGER
 );
 
 CREATE TABLE sub_genre (
+id INTEGER PRIMARY KEY,
+name TEXT
 );
 
 CREATE TABLE authors (
