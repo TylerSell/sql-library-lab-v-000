6aea1009e37cdc583c4dcb0b56cdaa0f02b37610
tylerselldesign <tylerselldesign@gmail.com>
2019-01-10 05:55:28 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+  );
+
+CREATE TABLE sub_genre (
+);
+
+CREATE TABLE authors (
+);
+
+CREATE TABLE books (
+);
+
+CREATE TABLE characters (
+);
+
+CREATE TABLE character_books (
+);
